63b0a70c74afda98f2e3219b675ca5d23ad3ee67
kunal kumar barman <kumar96kunal@gmail.com>
2019-10-18 07:06:41 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -4,10 +4,9 @@ import requests
 import webbrowser
 import sys
 from fake_useragent import UserAgent
-ua = UserAgent()
-header={'user-agent':ua.chrome}
+ua = {"UserAgent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0"}
 print("Googling.....")
-res=requests.get('https://www.google.com/search?q='+ ' '.join(sys.argv[1:]),headers=header)
+res=requests.get('https://www.google.com/search?q='+ ' '.join(sys.argv[1:]),headers=ua)
 #res.raise_for_status()
 file=open('project1a.html','wb') #only for knowing the class
 for i in res.iter_content(10000):
