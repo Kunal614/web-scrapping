9162981e8fd08a9d15e495baa3b219e76b7e33f9
kunal kumar barman <kumar96kunal@gmail.com>
2019-11-22 02:19:49 +0530

    Scrapped data of mobile details form flipkart

--- scrapy_project/flipkart/flipkart/spiders/flipcrt.py
+++ scrapy_project/flipkart/flipkart/spiders/flipcrt.py
@@ -9,16 +9,22 @@ class flip(scrapy.Spider):
         'https://www.flipkart.com/search?q=mobile&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off'
     ]
     def parse(self,response):
-        open_in_browser(response)
+        #open_in_browser(response)
         items =  FlipkartItem()
         mobile_name = response.css('._3wU53n').css('::text').extract()
         mobile_specification = response.css('.tVe95H').css('::text').extract()
         mobile_price = response.css('._2rQ-NK').css('::text').extract()
-
-        items['mobile_name']=mobile_name
-        items['mobile_specification']=mobile_specification
-        items['mobile_price']=mobile_price
+        k=0
+        y=0
+        print(len(mobile_name))
+        for i in range(len(mobile_name)):
+            items['mobile_name']=mobile_name[i]
+            items['mobile_specification']=mobile_specification[k]
+            items['mobile_price']=mobile_price[y]
+            k+=1
+            y+=1
+            yield items
         
-        yield items
+        
 
    
